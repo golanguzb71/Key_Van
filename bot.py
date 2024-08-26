b7474cc26e8e1dc7c2ad4b531cf1afe57c0042f8
golanguzb71 <abdulaziomonov4@gmail.com>
2024-08-26 15:32:51 +0500

    flask_up

--- bot.py
+++ bot.py
@@ -1,4 +1,28 @@
+from flask import Flask, request, jsonify
+import os
+import threading
 from helpers.helpers import bot, handle_contact_helper, send_welcome_helper, handle_renew_helper
+from utils.utils import process_doc
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+
+@app.route('/getTestTextFile', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+    if file:
+        print(file.filename)
+        return process_doc(file, file.filename)
+
+
+def run_flask():
+    app.run(host='0.0.0.0', port=5000)
 
 
 @bot.message_handler(commands=['start'])
@@ -16,5 +40,12 @@ def handle_renew(call):
     handle_renew_helper(call)
 
 
-if __name__ == "__main__":
+def run_bot():
     bot.polling()
+
+
+if __name__ == "__main__":
+    flask_thread = threading.Thread(target=run_flask)
+    flask_thread.daemon = True
+    flask_thread.start()
+    run_bot()
