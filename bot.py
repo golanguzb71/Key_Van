da11f3de12bc5c6d5435165239652ac157fdd61f
golanguzb71 <abdulaziomonov4@gmail.com>
2024-09-05 13:20:21 +0500

    long_pooling_added

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 import threading
-
+import time
 from flask import Flask, request, jsonify
-
 from helpers.helpers import bot, handle_contact_helper, send_welcome_helper, handle_renew_helper
 from utils.utils import process_doc
+from requests.exceptions import ReadTimeout
 
 app = Flask(__name__)
 
@@ -15,12 +15,14 @@ def upload_file():
     if file.filename == '':
         return jsonify({'error': 'No selected file'}), 400
     if file:
-        print(file.filename)
         return process_doc(file, file.filename)
 
 
 def run_flask():
-    app.run(host='0.0.0.0', port=5000)
+    try:
+        app.run(host='0.0.0.0', port=5000)
+    except Exception as e:
+        print(f"Error running Flask server: {e}")
 
 
 @bot.message_handler(commands=['start'])
@@ -39,11 +41,20 @@ def handle_renew(call):
 
 
 def run_bot():
-    bot.polling()
+    while True:
+        try:
+            bot.polling(timeout=60, long_polling_timeout=60)
+        except ReadTimeout:
+            print("Bot polling timed out. Retrying in 5 seconds...")
+            time.sleep(5)
+        except Exception as e:
+            print(f"An error occurred in the bot: {e}")
+            time.sleep(5)
 
 
 if __name__ == "__main__":
     flask_thread = threading.Thread(target=run_flask)
     flask_thread.daemon = True
     flask_thread.start()
+
     run_bot()
